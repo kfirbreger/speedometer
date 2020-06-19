4bd72b754bf434732282ec8d70c9c3c8050f5089
Kfir Breger <kfir@conopa.co>
2020-06-19 15:50:49 +0200

    Initial Commit

---
+++ speedometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  speedometer
+//
+//  Created by Kfir Breger on 19/06/2020.
+//  Copyright © 2020 Conopa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
